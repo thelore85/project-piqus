68c90e1adb9732f06b44e3e6d36caf48879c659f
TheLore85 <garofalo.digital@gmail.com>
2023-10-31 12:59:43 +0100

    feat: dashboard

---
+++ app/(admin)/layout.js
@@ -0,0 +1,29 @@
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { redirect } from 'next/navigation'
+export const dynamic = 'force-dynamic'; // force realtime update
+
+// components
+import Footer from '../Components/Footer/Footer'
+import NavbarLogIn from '../Components/Navbar/NavbarLogIn'
+
+
+
+// layout template
+export default async function layout({children}) {
+
+    const supabase = createServerComponentClient({ cookies })    
+    const { data } = await supabase.auth.getSession()
+  
+    if(!data.session){ redirect('/') } //protect the page from non logged-in users 
+
+  return (
+    <>
+      <NavbarLogIn />
+      <div className=" min-vh-100 container py-5">
+        {children}
+      </div>
+      <Footer/>
+    </>
+  )
+}
