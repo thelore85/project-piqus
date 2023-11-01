93fcbed85ae286268a83287ebe28ce2fb44eadf8
TheLore85 <garofalo.digital@gmail.com>
2023-11-01 22:41:03 +0100

    feat: about section

--- app/(pages)/home/CompHome/About.js
+++ app/(pages)/home/CompHome/About.js
@@ -1,20 +1,22 @@
 import Image from "next/image";
+import profilePic from "@/public/img/home/about_img.jpg"
 
 
 export default function About() {
   return (
-    <section className="p-5 text-center d-flex justify-content-center align-items-center" style={{ backgroundImage : 'url(/img/home/net-bg-1.jpg)', minHeight : '70%'}}>
+    <section className="p-4 text-center d-flex justify-content-center align-items-center" style={{ backgroundImage : 'url(/img/home/net-bg-1.jpg)', minHeight : '70%'}}>
       <div className="container ">
         
         <div className="">
-          <h1 className="text-white fw-bolder h1 p-5">Connecting people from more than 10 years</h1>
-            <div className="row p-4 d-flex align-items-center">
+          <h1 className="text-white fw-bolder h1 py-5">Connecting people from +10 years</h1>
+            <div className="row d-flex align-items-center">
               
               <div className="col-md-4">
-                <div className=" m-auto  rounded-circle" style={{ backgroundImage : "url(/img/home/about_img.jpg)", backgroundSize:'cover', backgroundPosition: 'top',  width: '100%', maxWidth: '300px', minHeight:'300px',  }}></div>
+                <Image  src={profilePic} width={250} className=" rounded-circle mb-4" alt="lorenzo garofalo"/>
+                {/* <div className=" m-auto  rounded-circle" style={{ backgroundImage : "url(/img/home/about_img.jpg)", backgroundSize:'cover', backgroundPosition: 'top',  width: '100%', maxWidth: '300px', minHeight:'300px',  }}></div> */}
               </div>
 
-              <div className="col-md-8 text-white text-start">
+              <div className="col-md-8 text-white text-start py-4">
                 <h4>Passion for digital</h4>
                 <p className="fw-lighter small">With a decade of experience in online communication, I bring an international perspective as a seasoned freelancer and collaborator with multinational companies in finance, healthcare, entertainment, and fashion. </p>
                 <p className="fw-lighter small">I&#39ve mastered modern online marketing strategies, implemented complex marketing technology, and kept websites and platforms up to date. </p>
