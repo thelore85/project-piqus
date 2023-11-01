92e8481d2abbc68280350e6d13b3137501a89da7
TheLore85 <garofalo.digital@gmail.com>
2023-11-01 22:23:49 +0100

    feat: home page new sections v3

--- app/(pages)/home/CompHome/About.js
+++ app/(pages)/home/CompHome/About.js
@@ -10,11 +10,11 @@ export default function About() {
           <h1 className="text-white fw-bolder h1 p-5">Connecting people from more than 10 years</h1>
             <div className="row p-4 d-flex align-items-center">
               
-              <div className="col-4">
+              <div className="col-md-4">
                 <div className=" m-auto  rounded-circle" style={{ backgroundImage : "url(/img/home/about_img.jpg)", backgroundSize:'cover', backgroundPosition: 'top',  width: '100%', maxWidth: '300px', minHeight:'300px',  }}></div>
               </div>
 
-              <div className="col-8 text-white text-start">
+              <div className="col-md-8 text-white text-start">
                 <h4>Passion for digital</h4>
                 <p className="fw-lighter small">With a decade of experience in online communication, I bring an international perspective as a seasoned freelancer and collaborator with multinational companies in finance, healthcare, entertainment, and fashion. </p>
                 <p className="fw-lighter small">I&#39ve mastered modern online marketing strategies, implemented complex marketing technology, and kept websites and platforms up to date. </p>
