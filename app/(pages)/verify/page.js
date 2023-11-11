6782a580d930546032fd48e4e2453ef432bc04f7
TheLore85 <garofalo.digital@gmail.com>
2023-11-11 01:25:34 +0100

    feat: email after signu

--- app/(pages)/verify/page.js
+++ app/(pages)/verify/page.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function page() {
   return (
-    <section className="h-75 p-2 d-flex align-items-center">
-      <div className='container text-center py-5'>
+    <section className="h-75 p-2 d-flex align-items-center justify-content-center " style={{ backgroundImage : 'url(/img/home/hero_bg.jpg)', backgroundSize : 'cover', backgroundPosition : 'center'}}>
+      <div className='text-center p-5 bg-white rounded'>
     
-        <h2 className="fs-2">Check you email!</h2>
-        <p className="fs-6">Thanks for sign up.<br />Go to your email inbox to validate the addres.</p>
+        <h2 className="fs-2">Check your email!</h2>
+        <p className="fs-6">Thanks for sign up.<br />Before start, go to your email inbox to validate the addres.</p>
 
       </div>
     </section>
