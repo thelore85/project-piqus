5d2393b329c9dcd167944923ca8b087accf07b05
TheLore85 <garofalo.digital@gmail.com>
2023-11-13 12:17:54 +0100

    feat: partner section v2

--- app/(pages)/home/CompHome/Partners.js
+++ app/(pages)/home/CompHome/Partners.js
@@ -13,11 +13,11 @@ export default function Partners() {
       <div className="container text-center py-5 ">
         <h5 className="text-black-50 small text-decoration-underline mb-4">I have worked with</h5>
         <Image alt="Banca Generali" width="80" src={bg} className="mx-4" style={{filter:"grayscale(100%)"}}/>
-        <Image alt="Pubblicita Progresso" width="80" src={pp} className="mx-4" style={{filter:"grayscale(100%)"}} />
-        <Image alt="Webank" width="80" src={webank} className="mx-4" style={{filter:"grayscale(100%)"}} />
-        <Image alt="Banco BPM" width="80" src={bpm} className="mx-4" style={{filter:"grayscale(100%)"}} />
-        <Image alt="Saxo Bank" width="80" src={saxo} className="mx-4" style={{filter:"grayscale(100%)"}} />
-        <Image alt="Saxo Bank" width="80" src={medicilio} className="mx-4" style={{filter:"grayscale(100%)"}} />
+        <Image alt="Pubblicita Progresso" width="80" src={pp} className="mx-4 mx-md-5 " style={{filter:"grayscale(100%)"}} />
+        <Image alt="Webank" width="80" src={webank} className="mx-3 mx-md-5" style={{filter:"grayscale(100%)"}} />
+        <Image alt="Banco BPM" width="80" src={bpm} className="mx-3 mx-md-5" style={{filter:"grayscale(100%)"}} />
+        <Image alt="Saxo Bank" width="80" src={saxo} className="mx-3 mx-md-5" style={{filter:"grayscale(100%)"}} />
+        <Image alt="Saxo Bank" width="80" src={medicilio} className="mx-3 mx-md-5" style={{filter:"grayscale(100%)"}} />
       </div>
     </section>
   )
