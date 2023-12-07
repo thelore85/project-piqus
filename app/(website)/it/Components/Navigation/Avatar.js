f1741631f1d5f5784a8ed4cab2d25dd67e81352b
TheLore85 <garofalo.digital@gmail.com>
2023-12-07 11:56:34 +0100

    feat: it/ profile migration to italina version

--- app/(website)/it/Components/Navigation/Avatar.js
+++ app/(website)/it/Components/Navigation/Avatar.js
@@ -2,10 +2,7 @@
 import Link from "next/link"
 import LogoutButton from "../Buttons/LogoutButton"
 import Image from "next/image";
-
-
 export const dynamic = 'force-dynamic' // force db refrech
-
 
 export default function Avatar({ session }) {
 
@@ -18,9 +15,9 @@ export default function Avatar({ session }) {
         <Image src="/img/user_img.jpg" alt="mdo" width="32" height="32" className="rounded-circle" />
       </div>
       <ul className="dropdown-menu text-small">
-        <li><Link className="dropdown-item border-bottom text-primary" href={ `/profile/${session?.user.id}`} >My Profile</Link></li>
-        <li><Link className="dropdown-item" href="/profile/newproject">New Project</Link></li>
-        <li><Link className="dropdown-item" href="/reset-password">Reset Password</Link></li>
+        <li><Link className="dropdown-item border-bottom text-primary" href={ `/it/profile/${session?.user.id}`} >My Profile</Link></li>
+        <li><Link className="dropdown-item" href="/it/profile/newproject">New Project</Link></li>
+        {/* <li><Link className="dropdown-item" href="/reset-password">Reset Password</Link></li> */}
         <li><Link href="#" className=" pointer-event dropdown-item text-black mt-3 py-2 border-top"><LogoutButton /></Link></li>
       </ul>
     </div>
---
+++ app/(website)/it/Components/UserProfile/ProjectList.js
@@ -0,0 +1,17 @@
+import ProjectCard from './ProjectCard';
+
+// component function
+export default function TktList({ tickets, onUpdate }) {
+
+  return (
+    <>
+      {tickets.map((ticket) => (
+        <ProjectCard key={ticket.id} ticket={ticket} onUpdate={onUpdate} />
+      ))}
+
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open tickets for this user!</p>
+      )}
+    </>
+  );
+}
