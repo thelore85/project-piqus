5fcd9380c8eab0fb89574218fb0e1e606f7a4d67
TheLore85 <garofalo.digital@gmail.com>
2023-12-08 23:35:35 +0100

    fix: auth redirect if logged in

---
+++ app/(website)/es/auth/layout.js
@@ -0,0 +1,25 @@
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { redirect } from 'next/navigation'
+export const dynamic = 'force-dynamic'; // force realtime update
+
+//components
+import Footer from './Components/Sections/Footer'
+import MainNavbar from './Components/Navigation/MainNavbar';
+
+
+export default async function authLayout({children}) {
+
+const supabase = createServerComponentClient({ cookies })    
+const { data } = await supabase.auth.getSession()
+
+if(data.session){ redirect(`/es/profile/${data.session.user.id}`) } //protect the page from logged-in users 
+
+  return (
+      <>
+        <MainNavbar />
+        {children}
+        <Footer />
+      </>
+  )
+}
